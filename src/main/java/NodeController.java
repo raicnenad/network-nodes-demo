035a8d87e3b48b99fbf469b784417d41b78cad03
Nenad Raic <raic.nenad@gmail.com>
2026-05-07 14:08:54 +0200

    dodavanje klasa

---
+++ src/main/java/NodeController.java
@@ -0,0 +1,17 @@
+package com.example.networknodesdemo;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/nodes")
+public class NodeController {
+
+    private final NodeRepository repository = new NodeRepository();
+    @GetMapping
+    public List<Node> getAllNodes() {
+        return repository.findAll();
+    }
+}
