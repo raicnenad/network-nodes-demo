7ba284f87b60c0f4f67eeaff2c6863cd8de03a67
Nenad Raic <raic.nenad@gmail.com>
2026-07-07 14:01:05 +0200

    dodavanje POST metode

--- src/main/java/com/example/network_nodes_demo/NodeController.java
+++ src/main/java/com/example/network_nodes_demo/NodeController.java
@@ -1,6 +1,8 @@
 package com.example.network_nodes_demo;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
@@ -18,5 +20,9 @@ public class NodeController {
     @GetMapping("/{id}")
     public Node getNodeById(@PathVariable Long id) {
         return service.getNodeById(id);
+    }
+    @PostMapping
+    public Node addNode(@RequestBody Node node) {
+        return service.addNode(node);
     }
 }
